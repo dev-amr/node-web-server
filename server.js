cb5a0bc17f9062d21beee8a1691c813f6842b872
amr-dev <amr0200@yahoo.com>
2018-11-08 00:19:12 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err)
+            console.log('Unable to append to server.log!');
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('Upper', (text) => {
+    return text.toUpperCase();
+});
+
+//serving static content
+app.use(express.static(__dirname + '/public'));
+
+//req handelers
+// app.get('/', (req, res) => {
+//     //res.send('<h1>Hellow from Express</h1>');
+//     res.send({
+//         name: 'Amr',
+//         age: 38,
+//         title: 'web dev'
+//     })
+// });
+
+// Templating Engine!
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: "welcome to the home page!"
+    })
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server starting on port 3000');
+});
